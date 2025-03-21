93fc4b62c3a91171e4dde5ef8b725641542b5c38
amitkumarsingh01 <aksmlibts@gmail.com>
2025-03-21 15:12:00 +0530

    Done

--- src/components/students/AddStudent.jsx
+++ src/components/students/AddStudent.jsx
@@ -123,7 +123,7 @@ const AddStudent = () => {
         </div>
         
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
         >
           Add Student
